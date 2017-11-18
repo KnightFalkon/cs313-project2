03927efa9aaca405e61adb098c6f164070a73255
KnightFalkon <jacobthehun@gmail.com>
2017-11-18 15:01:08 -0700

    testing to see if I can get the database to work

--- index.js
+++ index.js
@@ -1,10 +1,107 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var express = require('express');
+var app = express();
+
+var { Client } = require("pg"); // This is the postgres database connection module.
+
+
+const bcrypt = require('bcrypt');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/createPerson', function(request, response) {
+	createPerson(request, response);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function createPerson(request, response) {
+	// First get the person's id
+  var username = request.query.username;
+  var password = request.query.password;
+  var name = request.query.name;
+  var street = request.query.street;
+  var city = request.query.city;
+  var state = request.query.state;
+  var zip = request.query.zip;
+  var cardNum = request.query.cardNum;
+
+  hash = bcrypt.hashSync(password, 10);
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	createPersonOnDb(username, hash, name, street, city, state, zip, cardNum, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		// if (error || result == null || result.length != 1) {
+		// 	response.status(500).json({success: false, data: error});
+		// } else {
+		// 	var person = result[0];
+		// 	response.status(200).json(result[0]);
+    // }
+    console.log("user created");
+	});
+}
+
+function createPersonOnDb(username, hash, name, street, city, state, zip, cardNum, callback) {
+	//console.log("creating person on DB with id: " + id);
+
+	const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+    // var params = [id];
+    
+    var sql = "INSERT INTO users (username, hash, name, street, city, state, zip, cardNum) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+		var params = [username, hash, name, street, city, state, zip, cardNum];
+
+		var query = client.query(sql, params, function(err, result) {
+			// we are now done getting the data from the DB, disconnect the client
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+
+			//console.log("Found result: " + JSON.stringify(result.rows));
+
+			// logs whether it was successful
+			callback(null, "success");
+		});
+	});
+
+} // end of getPersonFromDb
