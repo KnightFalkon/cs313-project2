da8a4d621244c0d3899385bba64712d0bb45223c
KnightFalkon <jacobthehun@gmail.com>
2017-12-02 11:19:09 -0700

    trying to get sessions to work

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.session ({
+app.use(session ({
   secret: 'RagerGamer',
   resave: false,
   saveUninitialized: true
