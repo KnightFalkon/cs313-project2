7500bdb5db2f754a4d311422fac7c76fb440a8a8
KnightFalkon <jacobthehun@gmail.com>
2017-12-09 11:08:14 -0700

    yolo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var jquery = require('jquery');
-window.$ = window.jquery = jquery;
+global.$ = global.jquery = jquery;
 var { Client } = require("pg"); // This is the postgres database connection module.
 
 
