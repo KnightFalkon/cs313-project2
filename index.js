89a7db79c3742faf021d9fdda3018017d3e01a05
KnightFalkon <jacobthehun@gmail.com>
2017-12-12 13:38:59 -0700

    yolo

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.post('/login', function(req, res) {
       res.render('pages/browse');
     }
     else {
+			res.render('pages/signin');
       res.status(401).send({message: 'Username or Password is incorrect'});
     }
   });
